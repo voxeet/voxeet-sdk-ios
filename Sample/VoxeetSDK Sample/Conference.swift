cefa4ce3047395b490961121efc4a9026ef4eb58
Coco <co.larroque@gmail.com>
2016-05-03 17:55:19 +0200

    Sample

---
+++ Sample/VoxeetSDK Sample/Conference.swift
@@ -0,0 +1,177 @@
+//
+//  Conference.swift
+//  VoxeetSDK Sample
+//
+//  Created by Coco on 28/04/16.
+//  Copyright © 2016 Corentin Larroque. All rights reserved.
+//
+
+import UIKit
+import VoxeetSDK
+
+/*
+ *   MARK: - Conference class
+ */
+
+class Conference: UIViewController {
+    // UI.
+    @IBOutlet weak var conferenceIDLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var broadcastMessageTextView: UITextView!
+    
+    // Current conference ID.
+    var conferenceID: String?
+    
+    // Users.
+    var users = [String]()
+    
+    /*
+     *   MARK: Load
+     */
+    
+    override func viewDidLoad() {
+        // Setting label.
+        conferenceIDLabel.text = conferenceID
+        
+        // Joining / Launching demo.
+        if let confID = conferenceID {
+            // Joining Conference.
+            VoxeetSDK.sharedInstance.joinConference(conferenceAlias: confID) { (error) in
+                if error != nil {
+                    // Debug.
+                    print("::DEBUG:: <joinConference> \(error)")
+
+                    self.dismissViewControllerAnimated(true, completion: nil)
+                }
+            }
+        } else {
+            conferenceIDLabel.text = "Demo"
+            
+            // Creating Voxeet demo conference.
+            VoxeetSDK.sharedInstance.createDemoConference { (error) in
+                if error != nil {
+                    // Debug.
+                    print("::DEBUG:: <createDemoConference> \(error)")
+                    
+                    self.dismissViewControllerAnimated(true, completion: nil)
+                }
+            }
+        }
+        
+        // Conference delegate.
+        VoxeetSDK.sharedInstance.conferenceDelegate = self
+    }
+    
+    /*
+     *   MARK: Action
+     */
+    
+    @IBAction func sendBroadcastMessage(sender: AnyObject) {
+        // Alert view.
+        let alertController = UIAlertController(title: "Send Message", message: "Please input the message:", preferredStyle: .Alert)
+        
+        // Alert actions.
+        let confirmAction = UIAlertAction(title: "Send", style: .Default) { (_) in
+            if let textField = alertController.textFields?[0],
+                let message = textField.text {
+                // Sending a broadcast message.
+                VoxeetSDK.sharedInstance.sendMessage(message, completion: { (error) in
+                    // Debug.
+                    print("::DEBUG:: <sendBroadcastMessage> \(error)")
+                })
+            }
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel) { (_) in }
+        
+        // Alert textField.
+        alertController.addTextFieldWithConfigurationHandler { (textField) in
+            textField.placeholder = "Message"
+            textField.clearButtonMode = .WhileEditing
+        }
+        
+        alertController.addAction(confirmAction)
+        alertController.addAction(cancelAction)
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func hangUp(sender: AnyObject) {
+        VoxeetSDK.sharedInstance.leaveConference { (error) in
+            // Debug.
+            print("::DEBUG:: <leaveConference> \(error)")
+            
+            self.dismissViewControllerAnimated(true, completion: nil)
+        }
+    }
+}
+
+extension Conference: VTConferenceDelegate {
+    func userDidJoin(userID: String) {
+        users.append(userID)
+        tableView.reloadData()
+    }
+    
+    func userDidLeft(userID: String) {
+        users = users.filter({ $0 != userID })
+        tableView.reloadData()
+    }
+    
+    func messageReceived(userID: String, message: String) {
+        broadcastMessageTextView.text = "\(userID): \(message)"
+    }
+}
+
+/*
+ *   MARK: - Conference tableView dataSource & delegate
+ */
+
+extension Conference: UITableViewDataSource, UITableViewDelegate {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return users.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("tableViewCell", forIndexPath: indexPath) as! ConferenceTableViewCell
+        
+        let user = users[indexPath.row]
+        
+        cell.userLabel.text = user
+        
+        if let position = VoxeetSDK.sharedInstance.getUserPosition(user) {
+            cell.angleSlider.setValue(position.angle, animated: false)
+            cell.distanceSlider.setValue(position.distance, animated: false)
+        }
+        
+        return cell
+    }
+}
+
+/*
+ *   MARK: - Conference tableView cell
+ */
+
+class ConferenceTableViewCell: UITableViewCell {
+    // UI.
+    @IBOutlet weak var userLabel: UILabel!
+    @IBOutlet weak var angleSlider: UISlider!
+    @IBOutlet weak var distanceSlider: UISlider!
+    
+    /*
+     *   MARK: Action
+     */
+    
+    @IBAction func angle(sender: UISlider) {
+        // Debug.
+        print("::DEBUG:: <angle> \(sender.value)")
+        
+        // Setting user position.
+        VoxeetSDK.sharedInstance.setUserAngle(userLabel.text!, angle: sender.value)
+    }
+    
+    @IBAction func distance(sender: UISlider) {
+        // Debug.
+        print("::DEBUG:: <distance> \(sender.value)")
+        
+        // Setting user position.
+        VoxeetSDK.sharedInstance.setUserDistance(userLabel.text!, distance: sender.value)
+    }
+}
---
+++ Sample/VoxeetSDK Sample/ViewController.swift
@@ -0,0 +1,89 @@
+//
+//  ViewController.swift
+//  VoxeetSDK Sample
+//
+//  Created by Coco on 22/04/16.
+//  Copyright © 2016 Corentin Larroque. All rights reserved.
+//
+
+import UIKit
+import VoxeetSDK
+
+// NSUserDefaults.
+let VTConfID = "VTConfID"
+
+class ViewController: UIViewController {
+    
+    /*
+     *   MARK: Load
+     */
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    /*
+     *   MARK: Action
+     */
+    
+    @IBAction func createConference(sender: AnyObject) {
+        // Conference creation.
+        VoxeetSDK.sharedInstance.createConference(success: { (confID, confAlias) in
+            // Debug.
+            print("::DEBUG:: <createConference> \(confID), \(confAlias)")
+            
+            // Start the conference viewController.
+            self.presentConferenceVC(confAlias)
+            
+            }, fail: { (error) in
+                // Debug.
+                print("::DEBUG:: <createConference> \(error)")
+        })
+    }
+    
+    @IBAction func joinConference(sender: AnyObject) {
+        // Alert view.
+        let alertController = UIAlertController(title: "Conference ID", message: "Please input the conference ID:", preferredStyle: .Alert)
+        
+        // Alert actions.
+        let confirmAction = UIAlertAction(title: "Join", style: .Default) { (_) in
+            if let textField = alertController.textFields?[0],
+                let confID = textField.text {
+                
+                // Start the conference viewController.
+                self.presentConferenceVC(confID)
+                
+                // Save the current conference ID.
+                NSUserDefaults.standardUserDefaults().setObject(confID, forKey: VTConfID)
+                NSUserDefaults.standardUserDefaults().synchronize()
+            }
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel) { (_) in }
+        
+        // Alert textField.
+        alertController.addTextFieldWithConfigurationHandler { (textField) in
+            textField.placeholder = "Conference ID"
+            textField.keyboardType = .NumberPad
+            textField.clearButtonMode = .WhileEditing
+            
+            // Setting the textfield text by the previous text saved.
+            let text = NSUserDefaults.standardUserDefaults().stringForKey(VTConfID)
+            textField.text = text
+        }
+        
+        alertController.addAction(confirmAction)
+        alertController.addAction(cancelAction)
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    /*
+     *   MARK: Present conference viewController
+     */
+    
+    private func presentConferenceVC(confID: String) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let conferenceVC = storyboard.instantiateViewControllerWithIdentifier("Conference") as! Conference
+        conferenceVC.conferenceID = confID
+        self.presentViewController(conferenceVC, animated: true, completion: nil)
+    }
+}

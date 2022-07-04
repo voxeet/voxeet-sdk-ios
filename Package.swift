ee6b4553c8ebe61b0d6993f17c6269c4af415fc5
Yuriy Ganushevich <Yuriy.Ganushevich@dolby.com>
2022-07-04 16:52:45 +0200

    Update SPM with new voxeet-sdk-ios version 3.6.0-beta.1

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "VoxeetSDK",
     platforms: [
-        .iOS(.v11)
+        .iOS(.v12)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -28,15 +28,15 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "VoxeetSDK",
-            url: "https://vox-ios-sdk.s3.us-east-1.amazonaws.com/voxeetsdk/release/3.4.2/VoxeetSDK.zip",
-            checksum: "6106c66c6f031004287529bc57818efb25fa4774833e4198fc98be965812fede"),
+            url: "https://vox-ios-sdk.s3.us-east-1.amazonaws.com/voxeetsdk/release/3.6.0-beta.1/VoxeetSDK.zip",
+            checksum: "663e9419cbc07e9c53160571b4df29ca7956298a9a4f813fccc53347a7342122"),
         .binaryTarget(
             name: "WebRTC",
-            url: "https://vox-ios-sdk.s3.us-east-1.amazonaws.com/voxeetsdk/release/3.4.2/WebRTC.zip",
-            checksum: "8b2b680fa22a75ecd83ee3553fbe621f6133b6abe62da9e46b583a4fc4691dd0"),
+            url: "https://vox-ios-sdk.s3.us-east-1.amazonaws.com/voxeetsdk/release/3.6.0-beta.1/WebRTC.zip",
+            checksum: "501fe3131d2ac4e138d53edddfa5b0fb9ebc36cdfd90f239434cfc4f3a77459f"),
         .binaryTarget(
             name: "dvclient",
-            url: "https://vox-ios-sdk.s3.us-east-1.amazonaws.com/voxeetsdk/release/3.4.2/dvclient.zip",
-            checksum: "4115c97834c64ff8465b2ec9058345a61b5163a1afa8ce8e24d3397b5e04406d"),
+            url: "https://vox-ios-sdk.s3.us-east-1.amazonaws.com/voxeetsdk/release/3.6.0-beta.1/dvclient.zip",
+            checksum: "8f72ad188d6b66d285dc1de38e64fa44a1003a769c269a35ccb136304347a103"),
     ]
 )
